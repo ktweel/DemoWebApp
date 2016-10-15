671523a2bc398369f1362f1b1f2afcd69006c6e4
Kathryn Tweel <kathryntweel@Kathryns-MacBook-Pro-2.local>
2016-10-15 13:31:31 -0400

    add search box

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,28 @@ $(document).ready(function() {
 	getWeather();
 });
 
-function getWeather() {
-  var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&id=4930956&APPID=' + apiKey;
-  $.ajax(url, {
+function getWeather(data) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?';
+  var params = {
+  	APPID: apiKey,
+  	units: 'imperial'
+  };
+  if (searchQuery) {
+  	params.q = searchQuery;
+  } else {
+  	params.id = 4930956;
+  }
+
+  $.ajax(url + $.param(params), {
     success: function (data) {
       $('.city').text(data.name);
       $('.temp').text(data.main.temp + ' °F');
     }
   });
+}
+
+
+function searchWeatehr() {
+	var searchQuery = $('.search').val();
+	getWeather(searchQuery);
 }
