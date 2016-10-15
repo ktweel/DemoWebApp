4216393f2c5a2250f0c877f237352f53118ebbc2
Kathryn Tweel <kathryntweel@Kathryns-MacBook-Pro-2.local>
2016-10-15 13:25:09 -0400

    added weather api code

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,16 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
 }
 
+$(document).ready(function() {
+	getWeather();
+});
+
+function getWeather() {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&id=4930956&APPID=' + apiKey;
+  $.ajax(url, {
+    success: function (data) {
+      $('.city').text(data.name);
+      $('.temp').text(data.main.temp + ' °F');
+    }
+  });
+}
